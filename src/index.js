d2e11d7f5de5f41d12294b9ddee3af4536ef3ba6
Camden Brown <camden.brown@metova.com>
2018-01-22 14:28:42 -0600

    rendering base Search Bar component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
 const YOUTUBE_API_KEY = 'AIzaSyBzhtf19k_kI8l-sgtiynDXdIpMXIS9HiY';
 
 const App = () =>
 {
-  return <div>Hi</div>;
+  return (
+    <div>
+      <SearchBar/>
+    </div>
+  );
 };
 
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(<App/>, document.querySelector('.container'));
