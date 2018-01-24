7a9fc72bdf2b0e9e6f2ed1ac6b0ea52dbc532fb3
Camden Brown <camden.brown@metova.com>
2018-01-24 14:34:34 -0600

    make App a Class comp

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
-
+import YTSearch from 'youtube-api-search';
 const YOUTUBE_API_KEY = 'AIzaSyBzhtf19k_kI8l-sgtiynDXdIpMXIS9HiY';
 
-const App = () =>
-{
-  return (
-    <div>
-      <SearchBar/>
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
+
+    YTSearch({ key: YOUTUBE_API_KEY, term: 'surfboards' }, (videos) => {
+      this.setState({ videos }); //this.setState({ videos: videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar/>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
